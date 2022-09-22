0ac3899ab5833b1a7df090e0511908b82c961ef3
manikanta.kg <manikanta.kg@xelpmoc.in>
2022-09-22 10:56:10 +0530

    Practice Node.js basics

---
+++ app.js
@@ -0,0 +1,53 @@
+const number = '120'
+console.log(number);
+
+/* for(i=1;i<=10;i++){
+    console.log("5 multiple is ",i*5)
+}
+
+
+for(let i=1;i<=10;i++){
+    console.log("6 multiple is ",i*6)
+}
+
+let array = [1,2,3,4,5,"mani", 'kanta'];
+
+console.log(array);
+
+var j =0
+console.log(j) */
+
+// for (k=1;k<=13;k++){
+//     if(k)
+// }
+
+
+let a = 12;
+console.log(a);
+
+var b = 12;
+console.log(b);
+
+function letter(){
+    var a = "mkmkm";
+    console.log(a);
+}
+
+letter();
+
+function combine(){
+    var kk = 'naresh';
+
+    if (kk == 'naresh'){
+        let mm = 'manikanta';
+        console.log(kk+ ' '+ mm);
+    }
+    // console.log(kk+ ' '+ mm); //error
+}
+
+combine()
+
+
+
+
+
