442ce64b3a79b45edc9e2222895a224c99c4dc4d
manikanta.kg <manikanta.kg@xelpmoc.in>
2022-09-26 18:06:50 +0530

    events added

---
+++ event.js
@@ -0,0 +1,17 @@
+
+var events = require('events');
+var eventEmitter = new events.EventEmitter();
+ 
+// Create an event handler:
+var myEventHandler = function () {
+   
+    console.log('Welcome to GeeksforGeeks');
+}
+ 
+// Assign the event handler to an event:
+eventEmitter.on('geeks', myEventHandler);
+ 
+// Fire the 'geeks' event:
+eventEmitter.emit('geeks');
+
+myEventHandler()
